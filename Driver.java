8139fac18cb669264743d494c2410223ddbdb13c
Michael Millian <xenakaaii@gmail.com>
2013-11-12 16:39:50 -0500

    implemented a HashMap -MCM

--- Driver.java
+++ Driver.java
@@ -7,22 +7,22 @@ public class Driver{
 	}
 
 	public void go() throws IOException{
-		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("test.gr"))));
+		BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(new File("test2.gr"))));
 		String line=null;
-		//graph should be a hashmap
-		ArrayList<ArrayList<Tuple<Integer,Integer>>> graph= new ArrayList<ArrayList<Tuple<Integer,Integer>>>();
+		HashMap<Integer,ArrayList<Tuple<Integer,Integer>>> graph = new HashMap<Integer,ArrayList<Tuple<Integer,Integer>>>(10);
 		while ((line=br.readLine())!=null){
 			String[] elements=line.split(" ");
 			if (elements[0].equals("a")){
 				int firstVertex=Integer.valueOf(elements[1]);
-				System.out.println(graph.size());
-				System.out.println(firstVertex);
 				int secondVertex=Integer.valueOf(elements[2]);
 				int dist=Integer.valueOf(elements[3]);
-				if (firstVertex>graph.size()){
-					
-				}
+				if (graph.containsKey(firstVertex)){
 					graph.get(firstVertex).add(new Tuple<Integer,Integer>(secondVertex,dist));
+				} else {
+					ArrayList<Tuple<Integer,Integer>> a = new ArrayList<Tuple<Integer,Integer>>(10);
+					a.add(new Tuple<Integer,Integer>(secondVertex,dist));
+					graph.put(firstVertex,a);
+				}
 					
 			}
 			else if (elements[0].equals("c")){
@@ -31,19 +31,23 @@ public class Driver{
 			else if (elements[0].equals("p")){
 				System.out.println("Hello");
 				int size=Integer.valueOf(elements[2]);
-				System.out.println("size:"+size);
-				graph=new ArrayList<ArrayList<Tuple<Integer,Integer>>>(size);
-				System.out.println(graph.size());
-				graph.ensureCapacity(size);
+				graph = new HashMap<Integer,ArrayList<Tuple<Integer,Integer>>>(size*2);//make sure its big enough to avoid too many conflicts
 			}
 		}
+		br.close();
 		print(graph);
 	}
 	
-	public void print(ArrayList<ArrayList<Tuple<Integer,Integer>>> g){
-		for (ArrayList<Tuple<Integer,Integer>> vertex:g){
+	
+	
+	public void print(HashMap<Integer,ArrayList<Tuple<Integer,Integer>>> g){
+		Set<Integer> keys = g.keySet();
+		System.out.println("keys:"+keys.toString());
+		for(Integer i:keys){
+			ArrayList<Tuple<Integer,Integer>> vertex = g.get(i);
+			System.out.print(i+":\t");
 			for (Tuple<Integer,Integer>neighbor:vertex){
-				System.out.print(neighbor.getFirst()+"\t");
+				System.out.print("<"+neighbor.getFirst()+","+neighbor.getSecond()+">"+"\t");
 			}
 			System.out.println();
 		}
